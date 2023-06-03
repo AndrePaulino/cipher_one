8b29560204f745bb826f5bd15433f2be2dc0a7ce
Andre Paulino <andrepaulinobtw@gmail.com>
2023-06-03 19:45:29 -0300

    ✍🏽 Refactored and modularized cipher_keys

---
+++ scripts/cipher_keys.js
@@ -0,0 +1,9 @@
+const cipher_keys = {
+	a: "ai",
+	e: "enter",
+	i: "imes",
+	o: "ober",
+	u: "ufat",
+};
+
+export default cipher_keys;
--- scripts/encipher.js
+++ scripts/encipher.js
@@ -1,27 +1,23 @@
+import cipher_keys from "./cipher_keys";
+
 const re_special_char = /([^\w ]|[_])/g;
 const re_accents = /[\p{Diacritic}]/gu;
 
-const cipher_keys = {
-  a: "ai",
-  e: "enter",
-  i: "imes",
-  o: "ober",
-  u: "ufat",
-};
-
 const Encipher = (message) => {
-  let processed_msg = message.normalize("NFD");
-  processed_msg = processed_msg.replace(re_accents, "");
-  processed_msg = processed_msg.replace(re_special_char, "");
-  processed_msg = processed_msg.toLowerCase();
+	let processed_msg = message.normalize("NFD");
+	processed_msg = processed_msg.replace(re_accents, "");
+	processed_msg = processed_msg.replace(re_special_char, "");
+	processed_msg = processed_msg.toLowerCase();
 
-  let output = [...processed_msg].map((char) => {
-    if (Object.keys(cipher_keys).includes(char))
-      return (char = cipher_keys[char]);
-    return char;
-  });
+	let output = [...processed_msg].map((char) => {
+		if (Object.keys(cipher_keys).includes(char)) {
+			return (char = cipher_keys[char]);
+		}
 
-  return output.join("");
+		return char;
+	});
+
+	return output.join("");
 };
 
 export default Encipher;
